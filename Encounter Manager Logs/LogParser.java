283cb3b2b560944c2c5d477552353be860d894c9
irfanalgan <60664073+irfanalgan@users.noreply.github.com>
2022-02-24 20:01:05 +0200

    Log Parser

---
+++ Encounter Manager Logs/LogParser.java
@@ -0,0 +1,76 @@
+import java.io.FileNotFoundException;
+import java.util.*;
+import java.util.stream.*;
+import java.io.*;
+import java.util.function.*;
+
+import static java.util.Map.Entry.comparingByValue;
+import static java.util.stream.Collectors.toMap;
+
+public class LogParser {
+
+    public static void main(String[] args) throws FileNotFoundException {
+
+    //TODO: Read file into a list of player scores (List of PlayerScore)
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(new FileInputStream(new File("encounterLogs.csv"))));
+
+        Function<String, PlayerScore> mapToLogs = (line) -> {
+            String[] p = line.split(",");
+            return new PlayerScore(p[0], p[1], Integer.parseInt(p[2]), Integer.parseInt(p[3]), Integer.parseInt(p[4]), Integer.parseInt(p[5]), Integer.parseInt(p[6]));
+        };
+        List<PlayerScore> scores = br.lines().skip(1).map(mapToLogs).collect(Collectors.toList());
+
+    // Query 1: Damage received by the player named Chopper.
+    //TODO
+        System.out.println("Query 1:");
+        scores.stream()
+                .filter(s -> s.getPlayerName().equals("Chopper"))
+                .map(PlayerScore::getDamageReceived)
+                .forEach(System.out::println);
+        System.out.println("--------");
+
+    // Query 2: List of healers' names that did more healing than 300 (HealingDone > 300)
+    //TODO    
+        System.out.println("Query 2:");
+        scores.stream()
+                .filter(s -> s.getHealingDone() > 300)
+                .map(PlayerScore::getPlayerName)
+                .sorted()
+                .distinct()
+                .forEach(System.out::println);
+        System.out.println("--------");
+
+
+    // Query 3: Damage dealt per damage received ratio for tanks
+    //TODO
+        System.out.println("Query 3:");
+        scores.stream()
+                .filter(s -> s.getRole().equals("Tank"))
+                .map(s-> (double)s.getDamageDealt()/s.getDamageReceived())
+                .forEach(System.out::println);
+        System.out.println("--------");
+
+
+    // Query 4: The name of the damage dealer that dealt the least amount of damage per attack (DamageDealt/Attacks)
+    //TODO
+        System.out.println("Query 4:");
+        System.out.println(scores.stream()
+                .filter(s -> s.getRole().equals("DamageDealer"))
+                .min(Comparator.comparing(PlayerScore::getDamagePerAttack))
+                .get()
+                .getPlayerName());
+        System.out.println("--------");
+
+    // Query 5: How much healing was received by the tank that dealt the most damage 
+    //TODO
+        System.out.println("Query 5:");
+        System.out.println(scores.stream()
+                .filter(s -> s.getRole().equals("Tank"))
+                .max(Comparator.comparing(PlayerScore::getDamageDealt))
+                .get()
+                .getHealingReceived());
+        System.out.println("--------");
+    }
+
+}
---
+++ Encounter Manager Logs/PlayerScore.java
@@ -0,0 +1,87 @@
+public class PlayerScore {	
+    private String playerName;          // Name of the player
+    private String role;                // Role of player
+    private Integer damageDealt;        // Amount of damage dealt
+    private Integer damageReceived;     // Amount of damage received
+    private Integer healingDone;        // Amount of healing dealt
+    private Integer healingReceived;    // Amount of healing received
+    private Integer attacks;            // Number of attacks performed
+    PlayerScore(String name, String playerRole, int dmgDealt, int dmgReceived, int healDone, int healReceived, int numOfAttacks) {
+        playerName = name;
+        role = playerRole;
+        damageDealt = dmgDealt;
+        damageReceived = dmgReceived;
+        healingDone = healDone;
+        healingReceived = healReceived;
+        attacks = numOfAttacks;
+    }
+
+
+
+    //TODO
+    //Getters
+
+    /**
+     *  return damage per attack
+     * @return
+     */
+    public Double  getDamagePerAttack(){
+        return (double) damageDealt/damageReceived;
+    }
+
+    /**
+     * return player name
+     * @return
+     */
+    public String getPlayerName() {
+        return playerName;
+    }
+
+    /**
+     * return player role
+     * @return
+     */
+    public String getRole() {
+        return role;
+    }
+
+    /**
+     * return damage dealt
+     * @return
+     */
+    public Integer getDamageDealt() {
+        return damageDealt;
+    }
+
+    /**
+     * return damage received
+     * @return
+     */
+    public Integer getDamageReceived() {
+        return damageReceived;
+    }
+
+    /**
+     * return healing done
+     * @return
+     */
+    public Integer getHealingDone() {
+        return healingDone;
+    }
+
+    /**
+     * return healing received
+     * @return
+     */
+    public Integer getHealingReceived() {
+        return healingReceived;
+    }
+
+    /**
+     * return attacks
+     * @return
+     */
+    public Integer getAttacks() {
+        return attacks;
+    }
+}
